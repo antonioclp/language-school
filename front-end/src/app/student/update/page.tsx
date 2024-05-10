11817ca224c829679f6d3cbf4fcee12e7e4d4754
antonioclp <antonionetosg6@gmail.com>
2024-05-10 10:54:11 -0300

    feat: final implementatinons

--- front-end/src/app/student/update/page.tsx
+++ front-end/src/app/student/update/page.tsx
@@ -1,11 +1,18 @@
+import { updateStudent } from "@/utils/api";
+
 export default function UpdateStudent() {
   async function formAction(form: FormData) {
     "use server";
 
     const registration = form.get("registration");
-    const englishGrade = form.get("english-grade");
-    const portugueseGrade = form.get("portuguese-grade");
-    const japaneseGrarde = form.get("japanese-grade");
+
+    const updatedStudent = {
+      englishGrade: Number(form.get("english-grade") as string),
+      portugueseGrade: Number(form.get("portuguese-grade") as string),
+      japaneseGrade: Number(form.get("japanese-grade") as string),
+    };
+
+    updateStudent(Number(registration), updatedStudent);
   }
 
   return (
@@ -27,6 +34,7 @@ export default function UpdateStudent() {
           <span>Japanese Grade</span>
           <input type="number" id="japanese-grade" name="japanese-grade" />
         </label>
+        <button type="submit">Confirm</button>
       </form>
     </main>
   );
